6b684b2c383eeb6149499da9769654a0dbd076d2
13tomoore <tom.moore@outlook.fr>
2021-03-06 09:24:25 +0100

    Chargement dynamique des composants 1 et 2

--- main/main.cpp
+++ main/main.cpp
@@ -1,20 +1,49 @@
 
 #include <iostream>
-#include "Composant1.h"
-#include "Composant2.h"
+#include <dlfcn.h>
+#include <string.h>
 
 int main(int argc, char ** argv)
 {
-	int data1=3;
+    char *nomComposant = argv[1];
+    void *openComposant;
+    char *error;
+    
+    openComposant = dlopen (nomComposant, RTLD_LOCAL|RTLD_LAZY);
+    if (!openComposant) {
+        fputs (dlerror(), stderr);
+        exit(1);
+    }
+    
+    int data1=3;
 	int data2=5;
-
-	int valeur1;
-	int valeur2;
-
-	valeur1=composant1(data1,data2);
-
-	valeur2=composant2(data1,data2);
-
-	std::cout << getComposant1Version() << std::endl;
-	std::cout << "valeur 1 :" << valeur1 << " valeur 2 :" << valeur2 << std::endl;
+    
+    int (*composantFonction)(int,int);
+    int valeur;
+    char* (*composantVersion)();
+    char *version;
+    if(strcmp ("./libComposant1.so", nomComposant) == 0){
+        composantFonction = (int (*)(int, int)) dlsym(openComposant, "_Z10composant1ii");
+        if ((error = dlerror()) != NULL)  {
+            fputs(error, stderr);
+            exit(1);
+        }
+        composantVersion=(char* (*)()) dlsym(openComposant, "_Z20getComposant1Versionv");
+        if ((error = dlerror()) != NULL)  {
+            fputs(error, stderr);
+            exit(1);
+        }
+        version=composantVersion();    
+        std::cout << "Version pour composant " << nomComposant << " : " << version << std::endl;
+    }else if(strcmp ("./libComposant2.so", nomComposant) == 0){
+        composantFonction = (int (*)(int, int)) dlsym(openComposant, "_Z10composant2ii");
+        if ((error = dlerror()) != NULL)  {
+            fputs(error, stderr);
+            exit(1);
+        }
+    }
+    
+    valeur=composantFonction(data1,data2);    
+    std::cout << "Valeur pour composant " << nomComposant << " : " << valeur << std::endl;
+    
 }
